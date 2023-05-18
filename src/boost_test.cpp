cdfd0477440b48c52de09a4c0aa547b4de0768b7
aluminium8 <haneda.masaya1998@gmail.com>
2023-05-18 09:21:18 +0000

    boost test (add the path of include and library on workflow )

--- src/boost_test.cpp
+++ src/boost_test.cpp
@@ -3,7 +3,9 @@
 
 int main() {
   //バージョンの出力
+  std::cout<<"boost link ok!"<<std::endl;
   std::cout << "boost version:" << BOOST_VERSION << std::endl;
   std::cout << "boost lib version:" << BOOST_LIB_VERSION << std::endl;
+  
   return 0;
 }
